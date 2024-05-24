4e74aed9a03fb78ddcf3a8785f1750f0597e64cb
akrovvidi <122575272+akrovvidi@users.noreply.github.com>
2024-05-24 00:04:34 -0700

    Updated lab

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Lab8-Starter
+
+## Answer:
+- Graceful degradation focuses on ensuring that web applications provide an optimal experience on modern browsers and devices. However,they only remain functional on older or less capable environments. On the other hand, service workers are standalone scripts operating independently of the main web app by managing tasks like caching and network requests without blocking the main thread. This capability allows web applications to maintain core functionality even when some advanced features are unsupported or fail such as internet or network access. 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,7 +45,9 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ('serviceWorker' in navigator) {
   // B2. TODO - Listen for the 'load' event on the window object.
+
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
@@ -54,6 +56,15 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }).catch((error) => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+    });
+  }
 }
 
 /**
@@ -68,21 +79,31 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  const storedRecipes = localStorage.getItem('recipes');
+  if (storedRecipes) {
+    return JSON.parse(storedRecipes);
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  const recipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
+  return new Promise(async (resolve, reject) => {
+
   /**************************/
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
+  
   /**************************/
   // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
   //            declared above
+    for (const url of RECIPE_URLS) {
+
   // A5. TODO - Since we are going to be dealing with asynchronous code, create
   //            a try / catch block. A6-A9 will be in the try portion, A10-A11
   //            will be in the catch portion.
@@ -98,9 +119,25 @@ async function getRecipes() {
   //            if you have, then save the recipes to storage using the function
   //            we have provided. Then, pass the recipes array to the Promise's
   //            resolve() method.
+      try{
+        const response = await fetch(url);
+        const recipe = await response.json();
+        recipes.push(recipe);
+        if (recipes.length === RECIPE_URLS.length) {
+          saveRecipesToStorage(recipes);
+          resolve(recipes);
+        }
+      }
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+    catch(error){
+      console.error(`Failed to fetch recipe from ${url}: `, error);
+      reject(error);
+    }
+  }
+});
 }
+
 
 /**
  * Takes in an array of recipes, converts it to a string, and then
